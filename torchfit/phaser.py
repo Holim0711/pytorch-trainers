f366ac9ebbfdbd353cdecd6e45c57a1a9cf0d363
Holim Lim <ihl7029@europa.snu.ac.kr>
2019-10-04 02:00:45 +0900

    remove param check & add convert option

--- torchfit/phaser.py
+++ torchfit/phaser.py
@@ -1,10 +1,23 @@
 import torch
-from .utils import *
+from collections import abc
+
+
+def _convert_(x, device):
+    if isinstance(x, torch.Tensor):
+        return x.to(device)
+    if isinstance(x, abc.Mapping):
+        return {k: _convert_(x[k], device) for k in x}
+    if isinstance(x, tuple) and hasattr(x, '_fields'):
+        return type(x)(*(_convert_(v, device) for v in x))
+    if isinstance(x, abc.Sequence) and not isinstance(x, str):
+        return type(x)(_convert_(v, device) for v in x)
+    return x
 
 
 class Phaser():
 
-    def __init__(self, model, criterion, optimizer, device=None):
+    def __init__(self, model, criterion, optimizer, device=None,
+                 convert_x=None, convert_y=None):
         if device is not None:
             model.to(device)
             criterion.to(device)
@@ -15,17 +28,27 @@ class Phaser():
         self.optimizer = optimizer
         self.device = device
 
+        self.convert_x = convert_x if convert_x is not None else _convert_
+        self.convert_y = convert_y if convert_y is not None else _convert_
+
         self.callbacks = {
-            'train': None,
-            'valid': None,
+            'train': [],
+            'valid': [],
         }
+
+    def after(self, phase):
+        if phase not in self.callbacks:
+            raise ValueError(f'invalid phase name: {phase}')
+        def register(func):
+            self.callbacks[phase].append(func)
+        return register
 
     def train(self, dataloader):
         self.model.train()
 
         for x, y in dataloader:
-            x = _convert_data(x, self.device)
-            y = _convert_data(y, self.device)
+            x = self.convert_x(x, device=self.device)
+            y = self.convert_y(y, device=self.device)
 
             ŷ = self.model(x)
             l = self.criterion(ŷ, y)
@@ -34,27 +57,19 @@ class Phaser():
             l.backward()
             self.optimizer.step()
 
-            if self.callbacks['train'] is not None:
-                self.callbacks['train'](pred=ŷ, true=y, loss=l)
+            for func in self.callbacks['train']:
+                func(pred=ŷ, true=y, loss=l)
 
     @torch.no_grad()
     def valid(self, dataloader):
         self.model.eval()
 
         for x, y in dataloader:
-            x = _convert_data(x, self.device)
-            y = _convert_data(y, self.device)
+            x = self.convert_x(x, device=self.device)
+            y = self.convert_y(y, device=self.device)
 
             ŷ = self.model(x)
             l = self.criterion(ŷ, y)
 
-            if self.callbacks['valid'] is not None:
-                self.callbacks['valid'](pred=ŷ, true=y, loss=l)
-
-    def after(self, phase):
-        if phase not in self.callbacks:
-            raise ValueError(f'phase should be in {set(self.callbacks)}.')
-        def register(func):
-            _check_named_params(func, ['pred', 'true', 'loss'])
-            self.callbacks[phase] = func
-        return register
+            for func in self.callbacks['valid']:
+                func(pred=ŷ, true=y, loss=l)
